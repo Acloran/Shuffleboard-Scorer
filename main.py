1322985415386e86af436321802f417548c98004
Acloran <32380754+Acloran@users.noreply.github.com>
2022-07-05 18:29:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ cam = cv2.VideoCapture(0)#,cv2.CAP_DSHOW)
 
 cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)  # set new dimensionns to cam object (not cap)
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1024)
-cam.release()
-cv2.destroyAllWindows()
+
 #cam = cv2.imread('images/c1.png')
 
 def callback(x):
