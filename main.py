d5cf60ee5eebec02db8f6b0d27e7c0ca939a30e4
Acloran <32380754+Acloran@users.noreply.github.com>
2022-07-05 13:58:39 -0500

    increased resolution

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import numpy as np
 import cv2
 import keyboard
 
-cam = cv2.VideoCapture(0)
+cam = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)  # set new dimensionns to cam object (not cap)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
 while True:
     ret, frame = cam.read()
