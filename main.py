863a468f9232a2e20da5d11e25c0fb460e1a8e10
Acloran <32380754+Acloran@users.noreply.github.com>
2022-07-05 14:13:20 -0500

    added trackbars

--- main.py
+++ main.py
@@ -7,13 +7,53 @@ cam = cv2.VideoCapture(0)#,cv2.CAP_DSHOW)
 cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)  # set new dimensionns to cam object (not cap)
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1024)
 
+def callback(x):
+	global H_low,H_high,S_low,S_high,V_low,V_high
+	#assign trackbar position value to H,S,V High and low variable
+	H_low = cv2.getTrackbarPos('low H','controls')
+	H_high = cv2.getTrackbarPos('high H','controls')
+	S_low = cv2.getTrackbarPos('low S','controls')
+	S_high = cv2.getTrackbarPos('high S','controls')
+	V_low = cv2.getTrackbarPos('low V','controls')
+	V_high = cv2.getTrackbarPos('high V','controls')
+
+cv2.namedWindow('controls',2)
+cv2.resizeWindow("controls", 550,10);
+
+H_low = 0
+H_high = 179
+S_low= 0
+S_high = 255
+V_low= 0
+V_high = 255
+
+#create trackbars for high,low H,S,V 
+cv2.createTrackbar('low H','controls',0,179,callback)
+cv2.createTrackbar('high H','controls',179,179,callback)
+
+cv2.createTrackbar('low S','controls',0,255,callback)
+cv2.createTrackbar('high S','controls',255,255,callback)
+
+cv2.createTrackbar('low V','controls',0,255,callback)
+cv2.createTrackbar('high V','controls',255,255,callback)
+
 while True:
     ret, frame = cam.read()
 
-    cv2.circle(frame,(640,512),10,(255,0,0),2)
-    cv2.circle(frame,(600,512),10,(0,0,255),2)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    cv2.imshow('Camera', frame)
+    hsv_low = np.array([H_low, S_low, V_low], np.uint8)
+    hsv_high = np.array([H_high, S_high, V_high], np.uint8)
+
+	#making mask for hsv range
+    mask = cv2.inRange(hsv, hsv_low, hsv_high)
+    print (mask)
+    res = cv2.bitwise_and(frame, frame, mask=mask)
+
+    cv2.imshow('mask',mask)
+    cv2.imshow('res',res)
+
+    #cv2.imshow('Camera', frame)
 
     if cv2.waitKey(1) == ord('q'):
         break
