61ff2e9068226dc8304441f506f0c19b204ff2d8
Acloran <32380754+Acloran@users.noreply.github.com>
2022-07-05 14:03:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import numpy as np
 import cv2
 import keyboard
 
-cam = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+cam = cv2.VideoCapture(0)#,cv2.CAP_DSHOW)
 
-#cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)  # set new dimensionns to cam object (not cap)
-#cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1024)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)  # set new dimensionns to cam object (not cap)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1024)
 
 while True:
     ret, frame = cam.read()
