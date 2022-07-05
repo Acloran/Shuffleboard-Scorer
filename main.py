c77c091507e15d093134598721ab8cb265dbc8a3
Acloran <32380754+Acloran@users.noreply.github.com>
2022-07-05 18:28:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ cam = cv2.VideoCapture(0)#,cv2.CAP_DSHOW)
 
 cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)  # set new dimensionns to cam object (not cap)
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1024)
-
+cam.release()
+cv2.destroyAllWindows()
 #cam = cv2.imread('images/c1.png')
 
 def callback(x):
@@ -132,5 +133,5 @@ while True:
 
     if cv2.waitKey(1) == ord('q'):
         break
-#cam.release()
+cam.release()
 cv2.destroyAllWindows()
