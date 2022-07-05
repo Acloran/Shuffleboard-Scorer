76720bbabfa6ab53941ad956b00f1765457a90b6
Acloran <32380754+Acloran@users.noreply.github.com>
2022-07-05 13:56:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ cam = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cam.read()
+
+    cv2.circle(frame,(400,400),8,(255,0,0),2)
+
     cv2.imshow('Camera', frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) == ord('q'):
         break
 cam.release()
 cv2.destroyAllWindows()
