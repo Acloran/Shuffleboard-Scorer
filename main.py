9bef1b2b8a3f1e6a1b833deadb0e4390d5ebe670
Acloran <32380754+Acloran@users.noreply.github.com>
2022-07-05 19:25:09 -0500

    dropped min to 4

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ while True:
 
     #Drawing detected circles
     redcircles = cv2.HoughCircles(redMask,cv2.HOUGH_GRADIENT,1,2,
-                            param1=50,param2=20,minRadius=10,maxRadius=0)
+                            param1=50,param2=20,minRadius=4,maxRadius=0)
     if redcircles is not None:
         redcircles = np.uint16(np.around(redcircles))
         #print(redcircles)
