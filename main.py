1e958a9e5b84eb93bd7b6c4442892f5de511ed9a
Acloran <32380754+Acloran@users.noreply.github.com>
2022-07-05 19:20:04 -0500

    more visual changes

--- main.py
+++ main.py
@@ -113,7 +113,7 @@ while True:
         if radius>10 and radius<25:
             cv2.circle(outputImg,center,18,(255,0,0),2)
             # draw the center of the circle
-            cv2.circle(outputImg,center,4,(155,155,155),-1)
+            cv2.circle(outputImg,center,8,(195,195,195),-1)
 
     #making the red mask
 
