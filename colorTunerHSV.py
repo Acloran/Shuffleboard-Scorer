848e9364819515a771c67c080c2dfacd0f41f920
Acloran <32380754+Acloran@users.noreply.github.com>
2022-07-05 22:27:00 -0500

    non inverted lab sapce

--- colorTunerHSV.py
+++ colorTunerHSV.py
@@ -57,7 +57,7 @@ while True:
     
     #recolor Image
     frame = cv2.bitwise_not(dst)
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)
+    hsv = cv2.cvtColor(dst, cv2.COLOR_BGR2LAB)
     cv2.imshow('raw',dst)
     cv2.imshow('hsv',hsv) 
     cv2.imshow('inverted',frame)
