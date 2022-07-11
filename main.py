ae9b7aeafeacda287b84f1dcaccd3553f9a4276c
Acloran <32380754+Acloran@users.noreply.github.com>
2022-07-11 17:50:20 -0500

    casting to a string

--- main.py
+++ main.py
@@ -58,7 +58,8 @@ def drawRedorBlueCircle(imgin, imgout, x, y):
     # ret, mask = cv2.threshold(img2gray, 10, 255, cv2.THRESH_BINARY)
     # result = cv2.bitwise_and(imgin, imgin, mask=mask)
     circleColor = (255,255,255)
-    cv2.putText(imgout, blueVal, (int(x)+22,int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, circleColor, 2, cv2.LINE_AA)
+    strBlueVal = str(blueVal)
+    cv2.putText(imgout, strBlueVal, (int(x)+22,int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, circleColor, 2, cv2.LINE_AA)
     cv2.circle(imgout,center,18,circleColor,2)
     # draw the center of the circle
     cv2.circle(imgout,center,6,circleColor,-1)
