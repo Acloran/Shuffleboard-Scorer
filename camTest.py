f75baf2f71e614e6a222067e34fce5e608738014
Acloran <32380754+Acloran@users.noreply.github.com>
2022-07-18 02:32:09 -0500

    using picamera

--- camTest.py
+++ camTest.py
@@ -1,16 +1,25 @@
 #from doctest import OutputChecker
 import numpy as np 
 import cv2
+import time
+import picamera
 #import keyboard
 
-cam = cv2.VideoCapture(0)#,cv2.CAP_DSHOW)
+#cam = cv2.VideoCapture(0)#,cv2.CAP_DSHOW)
 
-cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)  # set new dimensionns to cam object (not cap)
-cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1536)
+#cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)  # set new dimensionns to cam object (not cap)
+#cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1536)
 
 while True:
-    ret, img = cam.read()
-    cv2.imshow('result',img)
+    with picamera.PiCamera() as camera:
+        camera.resolution = (1920, 1080)
+        camera.framerate = 3
+        time.sleep(2)
+        output = np.empty((1080, 1920, 3), dtype=np.uint8)
+        camera.capture(output, 'bgr')
+        image = image.reshape((1080, 1920, 3))
+        #ret, img = cam.read()
+    cv2.imshow('result',image)
 
     #cv2.imshow('Camera', frame)
 
@@ -19,5 +28,5 @@ while True:
             if cv2.waitKey(1) == ord('x'):
                 break
         break
-cam.release()
+
 cv2.destroyAllWindows()
